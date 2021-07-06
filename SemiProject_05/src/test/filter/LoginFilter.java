bf7ba7765f7e41bc0577763631c5d4d18537cc98
JellyFish <kurer456@naver.com>
2021-07-06 17:19:43 +0900

    loginfilter 수정 : notice 추가

--- SemiProject_05/src/test/filter/LoginFilter.java
+++ SemiProject_05/src/test/filter/LoginFilter.java
@@ -31,7 +31,7 @@ import javax.servlet.http.HttpSession;
  * 
  */
 @WebFilter(urlPatterns = 
-{"/users/private/*","/cafe/private/*"})
+{"/users/private/*","/cafe/private/*","/board/notice/private/*"})
 public class LoginFilter implements Filter{
 	@Override
 	public void destroy() {		
